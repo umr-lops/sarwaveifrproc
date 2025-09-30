b0689a0e888be30b9d1663e2e93aca3ce2b5fce5
agrouaze <antoine.grouazel@ifremer.fr>
2025-09-30 08:48:34 +0000

    remove method utils.load_models() and clean old unit test

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -55,57 +55,6 @@ for ii in subswathSAR:
     # hs_expected[ii] = get_hs_values(ff=ii)
     hs_expected.append(get_hs_values(ff=ii))
 
-
-# @pytest.mark.parametrize(
-#     "L1B_SAR_vv, hs_expected",
-#     [(L1B_SAR_vv[0], hs_expected[0])],
-# )
-# def test_hs_prediction(L1B_SAR_vv, hs_expected):
-#     from sarwaveifrproc.utils import load_config, load_models
-#
-#     ds = xr.open_dataset(
-#         L1B_SAR_vv
-#     )  # no group in this light l1B without interburst and without xspectra
-#     conf = load_config()
-#     predicted_variables = conf["predicted_variables"]
-#     paths = {
-#         "model_intraburst": conf["model_intraburst"],
-#         "model_interburst": conf["model_interburst"],
-#         "scaler_intraburst": conf["scaler_intraburst"],
-#         "scaler_interburst": conf["scaler_interburst"],
-#         "bins_intraburst": conf["bins_intraburst"],
-#         "bins_interburst": conf["bins_interburst"],
-#     }
-#     logging.info("Loading models...")
-#     (
-#         model_intraburst,
-#         model_interburst,
-#         scaler_intraburst,
-#         scaler_interburst,
-#         bins_intraburst,
-#         bins_interburst,
-#     ) = load_models(paths, predicted_variables)
-#     logging.info("Models loaded.")
-#     # kept_variables = ['corner_longitude', 'corner_latitude', 'land_flag', 'sigma0_filt', 'normalized_variance_filt',
-#     #                   'incidence', 'azimuth_cutoff', 'cwave_params']
-#     kept_variables = []
-#     dspredicted = sarwaveifrproc.l2_wave.generate_intermediate_product(
-#         ds,
-#         model_intraburst,
-#         scaler_intraburst,
-#         bins_intraburst,
-#         predicted_variables,
-#         kept_variables,
-#     )
-#     hs_actual = dspredicted["hs_mean"].data
-#     logging.info("hs_actual %s", hs_actual)
-#     logging.info("hs_expected %s", hs_expected)
-#     mask_nan = np.isfinite(hs_expected)
-#     diff = hs_actual[mask_nan] - hs_expected[mask_nan]
-#     logging.info("diff : minimal %s maximal %s", diff.min(), diff.max())
-#     assert np.allclose(hs_actual[mask_nan], hs_expected[mask_nan], atol=0.002)
-
-
 @pytest.mark.parametrize(
     "L1B_SAR_vv, hs_expected",
     [(L1B_SAR_vv[0], hs_expected[0])],
